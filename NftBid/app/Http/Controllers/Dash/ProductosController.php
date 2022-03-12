8e5607e3f844160885fe3a65dc2712f1d9a88ad1
Luis Grijalva <lgrijava@itsncg.edu.mx>
2022-03-11 18:18:48 -0700

    INSERT ROWS AND MODELS

--- NftBid/app/Http/Controllers/Dash/ProductosController.php
+++ NftBid/app/Http/Controllers/Dash/ProductosController.php
@@ -4,15 +4,62 @@ namespace App\Http\Controllers\Dash;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Validator;
+use App\Models\Nft;
+use Hash;
 
 class ProductosController extends Controller
 {
     public function miFuncion(){
         $categorias = \DB::table('categories')->get();
-        dd($categorias);
-        return view('dash.productos');
+        $productos =  \DB::table('nfts')->orderBy('id','DESC')->get();
+        //dd($categorias);
+        return view('dash.productos')
+            ->with('nfts',$productos)
+            ->with('categorias',$categorias);
     }
 
+    public function insertar(Request $req){
+        //dd($req);
+        $validacion = Validator::make($req->all(), [
+            'name'=>'required|min:4|max:100',
+            'description'=>'required|min:5',
+            'price'=>'required',
+            'img'=>'required|mimes:jpg,jpeg,png,webp|max:2000',
+            'btype'=>'required',
+            'cate'=>'required'
+        ]);
+        if($validacion->fails()){
+            return back()
+                ->withInput()
+                ->with('ErrorInsert','Favor de llenar todos los campos')
+                ->withErrors($validacion);
+        }else{
+            $ti=Hash::make(rand(0,9999999));
+            $ts=Hash::make(rand(0,9999999));
+            $img = $req->file('img');
+            $name = time(). '.' .$img->getClientOriginalExtension();
+            $destination_path=public_path('nfts');//se trae la carpeta public
+            $req->img->move($destination_path, $name);
+
+            $nuevo = Nft::create([
+                'name'=>$req->name,
+                'description'=>$req->description,
+                'base_price'=>$req->price,
+                'img'=>$name,
+                'blockchain_type'=>$req->btype,
+                'id_category'=>$req->cate,
+                'token_id'=>$ti,
+                'token_standar'=>$ts,
+                'metadata'=>'',
+                'id_user'=>1,
+                'likes'=>0
+            ]);
+            return back()->with('Listo','Se ha insertado correctamente');
+            
+        }//llave else
+
+    }//llave funcion
 
 
 }
--- NftBid/resources/views/dash/productos.blade.php
+++ NftBid/resources/views/dash/productos.blade.php
@@ -7,7 +7,47 @@
             <a href="#" data-toggle="modal" data-target="#modalAdd" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm"><i
                     class="fas fa-upload fa-sm text-white-50"></i> Agregar Producto</a>
         </div>
-        
+        <div class="row">
+            <!--SUCCESS  -->
+            @if($message = Session::get('Listo'))
+                <div class="row alert alert-success fade show">
+                    <h5 class="col-12"> <i class="fa fa-check"></i> Alerta</h5> 
+                    <br>
+                    <br>
+                    <p>{{ $message }}</p>
+                </div>
+            @endif
+            <!-- PRINT PRODUCTS -->
+            <div class="row">
+                @foreach($nfts as $nft)
+                    <div class="card col-3" >
+                        <img class="card-img-top" 
+                            src="{{ asset('/nfts/'.$nft->img) }}" 
+                            alt="Card image cap">
+                        <div class="card-body">
+                        <h5 class="card-title">{{ $nft->name }}</h5>
+                        <p class="card-text">{{ $nft->description }}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                @endforeach
+
+            </div>
+                
+
+        </div>
+        <!-- ERRORES-->
+        @if($message = Session::get('ErrorInsert'))
+            <div class="row alert alert-danger alert-dismissable fade show" role="alert">
+                <h5>Error: {{$message}}</h5>
+                <br><br>
+                <ul>
+                    @foreach($errors->all() as $error)
+                        <li>{{$error}}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
     </div>
     <!-- MODAL AGREGAR -->
     <div class="modal" tabindex="-1" role="dialog" id="modalAdd">
@@ -19,45 +59,54 @@
                 <span aria-hidden="true">&times;</span>
               </button>
             </div>
-            <div class="modal-body">
-                <div class="form-group">
-                    <label for="">Nombre</label>
-                    <input type="text" class="form-control" placeholder="Nombre Producto">
+            <form action="/admin/productos" method="POST" enctype="multipart/form-data">
+                @csrf
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="">Nombre</label>
+                        <input  type="text" class="form-control" 
+                            placeholder="Nombre Producto" name="name" 
+                            value="{{ old('name') }}">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Descripcion</label>
+                        <input type="text" class="form-control" placeholder="Descripcion" name="description"
+                        value="{{ old('description')}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Precio Base</label>
+                        <input type="number" class="form-control" placeholder="Precio Base" name="price"
+                        value="{{ old('price')}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Imagen</label>
+                        <input type="file" class="form-control" name="img"  
+                            value="{{ old('img')}}">
+                    </div>
+                    <div class="form-group">
+                        <label for="">Blockchain Type</label>
+                        <select name="btype" id=""  class="form-control"  value="{{ old('btype')}}">
+                            <option value="Etherium">Etherium</option>
+                            <option value="Polygon">Polygon</option>
+                            <option value="Klaytn">Klaytn</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="">Categoria</label>
+                        <select name="cate" id="" class="form-control"  value="{{ old('cate')}}">
+                            @foreach($categorias as $cate)
+                                <option value="{{ $cate->id }}">{{ $cate->category }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="">Descripcion</label>
-                    <input type="text" class="form-control" placeholder="Descripcion">
-                </div>
-                <div class="form-group">
-                    <label for="">Precio Base</label>
-                    <input type="number" class="form-control" placeholder="Precio Base">
-                </div>
-                <div class="form-group">
-                    <label for="">Imagen</label>
-                    <input type="file" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="">Blockchain Type</label>
-                    <select name="" id=""  class="form-control">
-                        <option value="Etherium">Etherium</option>
-                        <option value="Polygon">Polygon</option>
-                        <option value="Klaytn">Klaytn</option>
-                    </select>
-                </div>
-                <div class="form-group">
-                    <label for="">Categoria</label>
-                    <select name="" id="" class="form-control">
-                        
-                    </select>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-primary"> <i class="fa fa-save"></i> Guardar</button>
                 </div>
 
+            </form>
 
-
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancelar</button>
-              <button type="button" class="btn btn-primary"> <i class="fa fa-save"></i> Guardar</button>
-            </div>
           </div>
         </div>
     </div>
